47dff9543663093d9750233f2d24c2976f60ccf0
Aakash Chahal <aakash.chahal62@gmail.com>
2023-11-10 21:15:19 +0000

    add basic music card and controls structure

--- src/music_player/src/App.js
+++ src/music_player/src/App.js
@@ -1,51 +1,56 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 
 function App() {
+    // store a random music image url in a variable
+    const [imgUrl, setImgUrl] = React.useState("");
+    useEffect(() => {
+        const random_id = Math.floor(Math.random() * 100);
+        const url = `https://picsum.photos/id/${random_id}/300`;
+        setImgUrl(url);
+    }, []);
+
     return (
         <div className="App">
             <header className="App-header">
                 <div className="music-player">
-                    <div className="song-info">
-                        <p>Song Title</p>
-                        <p>Artist Name</p>
+                    <div
+                        className="song-info music-card"
+                        style={{
+                            backgroundImage: `url(${imgUrl})`,
+                            backgroundSize: "cover",
+                            backgroundPosition: "center",
+                            backgroundRepeat: "no-repeat",
+                        }}
+                    ></div>
+                    <div className="song-info-status">
+                        <div className="song-info-status-title">
+                            <h3>Music Title</h3>
+                            <p>Artist Name</p>
+                        </div>
+                        <div className="song-info-status-bar">
+                            <span style={{ width: "50%" }}></span>
+                        </div>
                     </div>
                     <div className="controls">
-                        <button
+                        <div
+                            className="controls-btn controls-btn-prev"
                             onClick={() => {
                                 window.alert("playing");
                             }}
-                        >
-                            Play
-                        </button>
-                        <button
+                        ></div>
+                        <div
+                            className="controls-btn controls-btn-play-pause"
                             onClick={() => {
                                 window.alert("playing");
                             }}
-                        >
-                            Pause
-                        </button>
-                        <button
+                        ></div>
+                        <div
+                            className="controls-btn controls-btn-next"
                             onClick={() => {
                                 window.alert("playing");
                             }}
-                        >
-                            Stop
-                        </button>
-                        <button
-                            onClick={() => {
-                                window.alert("playing");
-                            }}
-                        >
-                            Next
-                        </button>
-                        <button
-                            onClick={() => {
-                                window.alert("playing");
-                            }}
-                        >
-                            Previous
-                        </button>
+                        ></div>
                     </div>
                 </div>
             </header>
